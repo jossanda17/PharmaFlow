3010ee3b4d85a6594ab6ce14408e96d0cf845cb3
RizaSatt <136219635+RizaSatt@users.noreply.github.com>
2023-12-21 21:36:15 +0700

    Update API_penjualan.py

--- PharmaFlow/api/API_penjualan.py
+++ PharmaFlow/api/API_penjualan.py
@@ -5,6 +5,28 @@ from firebase_admin import firestore
 db = firestore.client()
 db_penjualan = db.collection('penjualan')
 
+def get_data_penjualan():
+    all_docs = {}
+    for doc in db_penjualan.stream():
+        all_docs[doc.id] = doc.to_dict()
+
+    return all_docs
+
+def get_data_penjualan_by_obat_id(obat_id):
+    all_docs = get_data_penjualan()
+
+    docs = {}
+    for doc in all_docs.items():
+        if doc[1]['obat_id'] == obat_id:
+            docs[doc[0]] = {
+                "day": doc[1]['day'],
+                "obat_id": doc[1]['obat_id'],
+                "obat_name": doc[1]['obat_name'],
+                "total_obat_amount": doc[1]['total_obat_amount']
+            }
+    
+    return docs
+
 API_penjualan = Blueprint('API_penjualan', __name__)
 
 @API_penjualan.route('/create', methods=['POST'])
@@ -15,12 +37,14 @@ def create():
         obat_id = request.get_json()['obat_id']
         obat_name = request.get_json()['obat_name']
         total_obat_amount = request.get_json()['total_obat_amount']
+
         db_penjualan.document(id.hex).set({
             "day": day,
             "obat_id": obat_id,
             "obat_name": obat_name,
             "total_obat_amount": total_obat_amount
         })
+
         return jsonify({
             "success": True
         }), 200
@@ -30,9 +54,15 @@ def create():
 @API_penjualan.route('/read')
 def read():
     try:
-        all_docs = {}
-        for doc in db_penjualan.stream():
-        	all_docs[doc.id] = doc.to_dict()
+        try: 
+            obat_id = request.args['obat_id'] 
+        except KeyError: 
+            obat_id = None
+
+        if obat_id == None:
+            all_docs = get_data_penjualan()
+        else:
+            all_docs = get_data_penjualan_by_obat_id(obat_id)
 
         return jsonify(all_docs), 200
     except Exception as e:
@@ -46,12 +76,14 @@ def update():
         obat_id = request.get_json()['obat_id']
         obat_name = request.get_json()['obat_name']
         total_obat_amount = request.get_json()['total_obat_amount']
+
         db_penjualan.document(id).update({
             "day": day,
             "obat_id": obat_id,
             "obat_name": obat_name,
             "total_obat_amount": total_obat_amount
         })
+
         return jsonify({
             "success": True
         }), 200
@@ -63,6 +95,7 @@ def delete():
     try:
         id = request.get_json()['id']
         db_penjualan.document(id).delete()
+
         return jsonify({
             "success": True
         }), 200
